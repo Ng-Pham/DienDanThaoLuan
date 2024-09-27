d597e0e0cb3aff35bff1e490146cbc8ed13ea5b0
Ng-Pham <nghipham2003@gmail.com>
2024-09-28 01:48:04 +0700

    update2

--- DienDanThaoLuan/Models/SoBaiChuDe.cs
+++ DienDanThaoLuan/Models/SoBaiChuDe.cs
@@ -7,7 +7,9 @@ namespace DienDanThaoLuan.Models
 {
     public class SoBaiChuDe
     {
+        public string MaLoai { get; set; }
         public string TenLoai { get; set; }
+        public string MaCD { get; set; }
         public string TenCD { get; set; }
         public int SoBai { get; set; }
     }
